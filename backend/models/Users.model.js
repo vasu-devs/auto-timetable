01ffcffc1596160fe5272cb4831db3c5ceb3a794
0xSYN.dev <pheonixgamer12345@gmail.com>
2025-12-05 02:15:08 +0530

    feat(backend): created student, techer

---
+++ backend/models/Users.model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  hashedPassword: {
+    type: String,
+    required: true,
+  },
+});
+
+const teacherSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  hashedPassword: {
+    type: String,
+    required: true,
+  },
+});
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  hashedPassword: {
+    type: String,
+    required: true,
+  },
+});
+
+const Admin = mongoose.model("Admin", adminSchema);
+const Teacher = mongoose.model("Teacher", teacherSchema);
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = {
+  Admin,
+  Teacher,
+  Student,
+};
